2d65bfb86064e7e5566e06e0b74b134cbcc32e85
indimon <90929596+indimon@users.noreply.github.com>
2023-02-17 06:32:40 -0500

    Delete igame directory

--- igame/config.py
+++
@@ -1,7 +0,0 @@
-from decouple import config
-
-DATABASE_URI = config('postgresql://postgres:password@igame-instance.cj3l9swcgrzl.us-east-1.rds.amazonaws.com/igame_db')
-
-
-class Config(object):
-    # needs work
